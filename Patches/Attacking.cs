44d0b1189ce84fc05726668a18b0a840da5a24de
SlejmUr <slejmur@protonmail.com>
2025-05-30 13:52:47 +0200

    patch attacking too

---
+++ Patches/Attacking.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace ExiledFixer.Patches;
+
+[HarmonyPatch(typeof(Exiled.Events.Patches.Events.Scp049.Attacking), nameof(Exiled.Events.Patches.Events.Scp049.Attacking.Transpiler))]
+internal static class Attacking
+{
+    [HarmonyPrefix]
+    public static bool Prefix(IEnumerable<CodeInstruction> instructions, ref IEnumerable<CodeInstruction> __result)
+    {
+        __result = instructions;
+        return false;
+    }
+}
