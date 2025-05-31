2b82f0729bd6e426051754791eb8412ad1aaa739
SlejmUr <slejmur@protonmail.com>
2025-05-31 22:23:19 +0200

    f

---
+++ Patches/PlacingTantrum.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace ExiledFixer.Patches;
+
+[HarmonyPatch(typeof(Exiled.Events.Patches.Events.Scp173.PlacingTantrum), nameof(Exiled.Events.Patches.Events.Scp173.PlacingTantrum.Transpiler))]
+internal static class PlacingTantrum
+{
+    [HarmonyPrefix]
+    public static bool Prefix(IEnumerable<CodeInstruction> instructions, ref IEnumerable<CodeInstruction> __result)
+    {
+        __result = instructions;
+        return false;
+    }
+}
